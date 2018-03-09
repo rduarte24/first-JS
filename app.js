9402241562d963b07158c4d8d1bb4895a784bef3
rduarte24 <ruben9_24@hotmail.com>
2018-03-09 16:50:03 -0500

    confirm

---
+++ app.js
@@ -0,0 +1,15 @@
+//Variable using Var Statement.
+
+var primerNombre = 'Juan';      //A variable name using camelcase.
+var primer_nombre = 'Juan';     //A variable name using underscore.
+var PrimerNombre = 'Juan';      //A variable name using Pascal Case.
+
+/*Variable using let and cons Statement.
+Using Let you cant re-declare the same Variable using Let
+You should use Let if you're going to change the variable value on the run.
+*/
+
+//Let example
+let name = 'Martha';
+console.log(name);
+
